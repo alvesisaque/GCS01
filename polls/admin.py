e01910fb4a7200d8388b22db27062244c4da0369
alvesisaque <alvesisaque838.ia@gmail.com>
2017-08-26 16:56:53 -0300

    Correcting errors

--- polls/admin.py
+++ polls/admin.py
@@ -15,7 +15,7 @@ class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date'],
-        'classes': ['collapse']}),
+                              'classes': ['collapse']}),
     ]
     inlines = [ChoiceInline]
     list_display = ('question_text', 'pub_date', 'was_published_recently')
